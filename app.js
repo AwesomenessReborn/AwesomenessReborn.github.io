fd02af06c0cde2751cc8e9bff1cbdc2b9984818a
Harinandan Kotamsetti <hareee234@gmail.com>
2023-01-17 12:01:58 -0800

    make change to the effect

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ async function rainbow() {
     await delayedColorChange('green', 1000); 
     await delayedColorChange('blue', 1000); 
     await delayedColorChange('violet', 1000); 
+    rainbow(); 
 }
 
 rainbow(); 
