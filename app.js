5d5e8060f6bc2d851712b8d991957904f08b6a95
Harinandan Kotamsetti <hareee234@gmail.com>
2023-02-18 14:18:38 -0800

    final comit

--- app.js
+++ app.js
@@ -14,12 +14,15 @@ const rgb_values = [
 async function rainbow() {
     
 
-    await delayedColorChange('#11151c', 1000); 
-    await delayedColorChange('#11151c', 1000); 
-    await delayedColorChange('#11151c', 1000); 
-    await delayedColorChange('#11151c', 1000); 
-    await delayedColorChange('#11151c', 1000); 
-    await delayedColorChange('#11151c', 1000); 
+    await delayedColorChange('#3a015c', 1000); 
+    await delayedColorChange('#32004f', 1000); 
+    await delayedColorChange('#220135', 1000); 
+    await delayedColorChange('#190028', 1000); 
+    await delayedColorChange('#11001c', 1000); 
+    await delayedColorChange('#190028', 1000); 
+    await delayedColorChange('#220135', 1000); 
+    await delayedColorChange('#32004f', 1000); 
+    await delayedColorChange('#3a015c', 1000); 
     rainbow(); 
 }
 
